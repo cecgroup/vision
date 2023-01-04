63023919fc8493f53175c4863767e21ebd378324
root <root@vision.vitepress>
2023-01-04 07:08:49 +0000

    1

---
+++ docs/.vitepress/dist/assets/suggestion.md.5052cd82.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s}from"./app.5dcc1ec2.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"suggestion.md"}'),o={name:"suggestion.md"};function a(n,r,c,i,p,_){return s(),t("div")}const f=e(o,[["render",a]]);export{m as __pageData,f as default};
---
+++ docs/.vitepress/dist/assets/vision.md.b7e04837.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./app.5dcc1ec2.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"vision.md"}'),a={name:"vision.md"};function s(n,r,c,i,p,_){return o(),t("div")}const f=e(a,[["render",s]]);export{m as __pageData,f as default};
