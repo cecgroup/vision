fea0afd264200584592513c4163fab6df0d968dd
root <root@vision.vitepress>
2023-01-04 03:38:37 +0000

    first

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+export default {
+    title: '工一',
+    description: 'CEC',
+    base: '/vision/',
+
+    
+    themeConfig: {
+      algolia: {
+        appId: 'N26XKP71HQ', // 需要替换
+        apiKey: 'b68973e86f1cea6fe107eecb587b3d40', // 需要替换
+        indexName: 'vision-index', // 需要替换
+        placeholder: '请输入关键词',
+        buttonText: '搜索'
+      }
+    }
+
+    
+}
